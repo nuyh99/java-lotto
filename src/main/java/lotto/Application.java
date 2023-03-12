9d6cbd095bc64edb1b40fb261cd51fe1ff1c8844
onegqueen <rkdwldnjs30@naver.com>
2023-03-12 22:12:37 +0900

    Feat: 추가 User 클래스
    set Money(), setLotto() 메소드 포함

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,42 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Application {
+    public static class User {
+        private int money;
+        private ArrayList<Integer>[] userLotto;
+
+        public User () {
+        }
+
+        private void setMoney() throws IllegalArgumentException {
+            this.money = Integer.parseInt(Console.readLine());
+            if (money % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 금액은 1,000원 단위여야 합니다.");
+            }
+        }
+
+        private void setLotto(int money){
+            userLotto = new ArrayList[money/1000];
+            System.out.println(money/1000+"개를 구매했습니다.");
+            for(int i=0;i<(money/1000);i++){
+                List <Integer> numbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+                userLotto[i]=new ArrayList<Integer>(numbers);
+                System.out.println(numbers);
+            }
+        }
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        User user = new User();
+        user.setMoney();
+        user.setLotto(user.money);
     }
 }
