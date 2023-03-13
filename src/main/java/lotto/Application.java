e6f6f57fce27ba59bc2dbf6a09291f5454c0d19a
onegqueen <rkdwldnjs30@naver.com>
2023-03-13 16:16:11 +0900

    Feat: 추가 PrintGameResult()
     로또당첨 결과를 출력하고 수익을 리턴하는 함수

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -11,6 +11,21 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Application {
+
+    private static double printGameResult(ArrayList<Integer> list) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+
+        System.out.println("3개 일치 (5,000원) - " + list.get(0) + "개");
+        System.out.println("4개 일치 (50,000원) - " + list.get(1) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + list.get(2) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + list.get(3) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + list.get(4) + "개");
+
+        return list.get(0) * 5000.0 + list.get(1) * 50000.0 + list.get(2) * 1500000.0
+            + list.get(3) * 30000000.0 + list.get(4) * 2000000000.0;
+    }
+
     private static class User {
 
         private int money;
