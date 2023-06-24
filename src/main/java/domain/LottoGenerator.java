ead654c2d28c96d0d26a46c63736ae0874d19ea2
howudong <53307093+howudong@users.noreply.github.com>
2023-06-24 23:28:16 +0900

    feat($LottoGenerator) : 로또 번호 생성기 클래스 추가

---
+++ src/main/java/domain/LottoGenerator.java
@@ -0,0 +1,12 @@
+package domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class LottoGenerator {
+    final public Lotto getLotto(int minNum, int maxNum, int count) {
+        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(minNum, maxNum, count);
+        return new Lotto(lotto);
+    }
+}
