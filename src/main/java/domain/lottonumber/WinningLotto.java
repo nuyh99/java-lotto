9202b6f904a35fbe518b8de2cdfaa4c407ce55b3
howudong <53307093+howudong@users.noreply.github.com>
2023-06-25 23:32:54 +0900

    feat ($WinningLotto) : 당첨 로또를 만드는 생성자 및 예외처리
    문자열을 입력받아 List<Integer>로 바꿈

--- src/main/java/domain/lottonumber/WinningLotto.java
+++ src/main/java/domain/lottonumber/WinningLotto.java
@@ -1,19 +1,56 @@
 package domain.lottonumber;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public final class WinningLotto extends Lotto {
     private final int bonus;
 
-    public WinningLotto(List<Integer> numbers, int bonus) {
-        super(numbers);
+    public WinningLotto(String readSentence, String bonus) {
+        super(getNumbersFromSentence(readSentence));
         validateBonus(bonus);
-        this.bonus = bonus;
+        this.bonus = Integer.parseInt(bonus);
     }
 
-    private void validateBonus(int bonus) {
-        if (NUMBER_LOWER_BOUND <= bonus && bonus <= NUMBER_MAX_BOUND) {
+    private static List<Integer> getNumbersFromSentence(String readSentence) {
+
+        try {
+            String[] splitNumber = readSentence.split(",");
+            return getLottoNumbers(splitNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("정수만 입력 가능합니다");
+        }
+    }
+
+    private static List<Integer> getLottoNumbers(String[] readNumbers) throws NumberFormatException {
+        return Arrays.stream(readNumbers)
+                .mapToInt(Integer::valueOf)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+
+    private void validateBonus(String bonus) {
+        if (!isNumeric(bonus)) {
+            throw new IllegalArgumentException("보너스 번호는 숫자만 가능합니다");
+        }
+        if (!isInRange(bonus)) {
             throw new IllegalArgumentException("유효하지 않은 보너스 번호");
         }
+    }
+
+    private static boolean isInRange(String bonus) {
+        int number = Integer.parseInt(bonus);
+        return (NUMBER_LOWER_BOUND <= number && number <= NUMBER_MAX_BOUND);
+    }
+
+    private boolean isNumeric(String bonus) {
+        try {
+            Integer.parseInt(bonus);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
     }
 }
--- src/test/java/domain/WinningLottoTest.java
+++ src/test/java/domain/WinningLottoTest.java
@@ -5,15 +5,45 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 class WinningLottoTest {
     @DisplayName("보너스 번호가 1 ~ 45가 아니면 예외가 발생한다.")
     @Test
     void createBonusByOutOfRange() {
-        Assertions.assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5), 49))
+        Assertions.assertThatThrownBy(() ->
+                        new WinningLotto("1,2,3,4,6,7", "49"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
 
+    @DisplayName("당첨 로또 번호에 문자가 존재하면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByWords() {
+        Assertions.assertThatThrownBy(() ->
+                        new WinningLotto("1,2,3,a,6,7", "40"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 로또 번호가 6자리가 넘으면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByOverSize() {
+        Assertions.assertThatThrownBy(() ->
+                        new WinningLotto("1,2,3,5,6,7,8,9", "40"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 로또 번호가 6자리 미만이면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByUnderSize() {
+        Assertions.assertThatThrownBy(() ->
+                        new WinningLotto("1,2,3,7,5", "40"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 로또 입력에 ',' 를 잘못 입력했을 경우")
+    @Test
+    void createWinningLottoByIncorrectRegex() {
+        Assertions.assertThatThrownBy(() ->
+                        new WinningLotto("1,2,3.75", "40"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 }
