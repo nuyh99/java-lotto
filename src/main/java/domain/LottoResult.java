691bfb16afb40be7f4248f9e4e7abb11b51545fb
howudong <53307093+howudong@users.noreply.github.com>
2023-06-28 20:14:47 +0900

    feat($LottoResult) : 로또 당첨 결과와 평균 수익을 저장하는 LottoResult 생성

--- src/main/java/domain/LottoResult.java
+++ src/main/java/domain/LottoResult.java
@@ -1,4 +1,38 @@
 package domain;
 
-public class LottoResult {
+import domain.lottonumber.Lotto;
+import domain.lottonumber.WinningLotto;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class LottoResult {
+    private static final String HALF_UP_AT_SECOND = "0.#";
+    private static final List<LottoRank> lottoResults = new ArrayList<>();
+
+    LottoResult(List<Lotto> lotto, WinningLotto winningLotto) {
+        for (Lotto currentLotto : lotto) {
+            lottoResults.add(winningLotto.checkRank(currentLotto));
+        }
+    }
+
+    public int getNumberOfRanks(LottoRank rank) {
+        return (int) lottoResults.stream()
+                .filter(e -> e == rank)
+                .count();
+    }
+
+    public String getAverageProfit() {
+        double sum = 0;
+        for (LottoRank lottoRank : lottoResults) {
+            sum += lottoRank.getMoney();
+        }
+        DecimalFormat decimalFormat = new DecimalFormat(HALF_UP_AT_SECOND);
+        decimalFormat.setRoundingMode(RoundingMode.HALF_UP);
+        return decimalFormat.format(sum);
+    }
+
+
 }
