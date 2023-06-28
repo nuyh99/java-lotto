2f5a63cfae82ad293d353e1e1105fc28455bc22b
howudong <53307093+howudong@users.noreply.github.com>
2023-06-28 10:52:51 +0900

    refactor($Computer) : 매직 넘버 LOTTO_PRICE 상수로 치환

--- src/main/java/domain/Computer.java
+++ src/main/java/domain/Computer.java
@@ -1,17 +1,31 @@
 package domain;
 
-public final class Computer {
-    public static final int LOTTO_PRICE = 1000;
-    private final int price;
+import domain.lottonumber.Lotto;
+import domain.lottonumber.LottoGenerator;
+import domain.lottonumber.WinningLotto;
 
-    public Computer(int price) {
-        validPrice(price);
-        this.price = price;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public final class Computer {
+    private static final int LOTTO_PRICE = 1000;
+
+    public LottoResult checkLottoRank(List<Lotto> lottos, WinningLotto winningLotto) {
+        for (Lotto lotto : lottos) {
+        }
     }
 
-    private void validPrice(int price) {
+    public List<Lotto> buyLottoByPrice(int price) {
         if (price % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("[ERROR] 입력 금액은 1000 단위만 가능합니다");
         }
+        int amount = price / LOTTO_PRICE;
+
+        final LottoGenerator lottoGenerator = new LottoGenerator();
+
+        return Stream.generate(lottoGenerator::get)
+                .limit(amount)
+                .collect(Collectors.toList());
     }
 }
