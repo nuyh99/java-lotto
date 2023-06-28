2560a84e610abf9eaa237a5ce0a40cacb2edb5da
howudong <53307093+howudong@users.noreply.github.com>
2023-06-28 15:53:06 +0900

    Test($Computer) : price가 1000가 아닐시 예외처리 되도록 함

--- src/main/java/domain/Computer.java
+++ src/main/java/domain/Computer.java
@@ -10,7 +10,6 @@ import java.util.stream.Stream;
 public final class Computer {
     private static final int LOTTO_PRICE = 1000;
 
-
     public List<Lotto> buyLottoByPrice(int price) {
         if (price % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("[ERROR] 입력 금액은 1000 단위만 가능합니다");
--- src/test/java/domain/ComputerTest.java
+++ src/test/java/domain/ComputerTest.java
@@ -1,5 +1,18 @@
 package domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class ComputerTest {
+
+    @DisplayName("가격이 1000단위가 아니면 얘외가 발생한다.")
+    @Test
+    void buyLottoByInvalidPrice() {
+
+        assertThatThrownBy(() -> new Computer().buyLottoByPrice(1040))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
 }
