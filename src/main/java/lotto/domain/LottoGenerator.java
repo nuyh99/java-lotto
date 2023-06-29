c474825d2326bc02caeadd206c3be349e1b3d59e
howudong <53307093+howudong@users.noreply.github.com>
2023-06-29 19:02:02 +0900

    fix($LottoGenerator) : UnsupportedOperationException가 발생하지 않도록 수정

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,9 +12,10 @@ public final class LottoGenerator {
     public static final int NUMBER_MAX_BOUND = 45;
     public static final int LOTTO_NUMBER_SIZE = 6;
 
-    static List<Integer> get() {
-        List<Integer> lotto = Randoms.pickUniqueNumbersInRange(NUMBER_LOWER_BOUND, NUMBER_MAX_BOUND, LOTTO_NUMBER_SIZE);
+    static List<Integer> get() throws IllegalArgumentException {
 
+        List<Integer> pickNumbers = Randoms.pickUniqueNumbersInRange(NUMBER_LOWER_BOUND, NUMBER_MAX_BOUND, LOTTO_NUMBER_SIZE);
+        List<Integer> lotto = new ArrayList<>(pickNumbers);
         Collections.sort(lotto);
         return lotto;
     }
